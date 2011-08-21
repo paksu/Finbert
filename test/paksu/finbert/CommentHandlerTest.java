5f5a49715dfc033b56d48818f62d8c843b1adc1a
Joonas Lehtonen <lehton83@gmail.com>
2011-08-21 16:40:34 +0300

    changed test location

---
+++ test/paksu/finbert/CommentHandlerTest.java
@@ -0,0 +1,18 @@
+package paksu.finbert;
+
+import org.junit.Test;
+
+public class CommentHandlerTest {
+	@Test
+	public void testSetComment() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComments() {
+		// System.out.println("Testing setComment()");
+		// CommentHandler ch = new CommentHandler();
+		// assertNotNull(ch.getComments("2011-08-18"));
+	}
+
+}
