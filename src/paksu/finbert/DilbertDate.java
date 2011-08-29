e34dc670fbbd715d44e0b64bd30ee9bb2e75f450
Joonas Lehtonen <lehton83@gmail.com>
2011-08-29 16:10:11 +0300

    hashCode and equels for DilberDate

--- src/paksu/finbert/DilbertDate.java
+++ src/paksu/finbert/DilbertDate.java
@@ -49,15 +49,33 @@ public final class DilbertDate {
 		return new DilbertDate(newest);
 	}
 
-	public static DilbertDate forDateOrNextBeforeDate(DateTime dt) {
-		throw new UnsupportedOperationException("not implemented");
-	}
-
-	public static DilbertDate forDateOrNextAfterDate(DateTime dt) {
+	public static DilbertDate exactlyForDate(DateTime dt) {
 		throw new UnsupportedOperationException("not implemented");
 	}
 
 	public static DilbertDate nearestToDate(DateTime dt) {
 		throw new UnsupportedOperationException("not implemented");
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+
+		if (obj == null || !(obj instanceof DilbertDate)) {
+			return false;
+		}
+
+		DilbertDate other = (DilbertDate) obj;
+		return other.date.equals(date);
 	}
 }
