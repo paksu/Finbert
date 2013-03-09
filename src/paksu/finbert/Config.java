d8fd830241d1326631e30efe00f3171ea0cfbd5d
Joonas Lehtonen <lehton83@gmail.com>
2013-03-09 11:22:38 +0200

    Added FinbertApplication

---
+++ src/paksu/finbert/Config.java
@@ -0,0 +1,5 @@
+package paksu.finbert;
+
+public class Config {
+	public static final float BITMAP_CACHE_PORTION = 0.2f;
+}
---
+++ src/paksu/finbert/FinbertApplication.java
@@ -0,0 +1,21 @@
+package paksu.finbert;
+
+import paksu.finbert.ui.utils.BitmapCache;
+import android.app.ActivityManager;
+import android.app.Application;
+
+public class FinbertApplication extends Application {
+
+	private BitmapCache bitmapCache;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+		bitmapCache = new BitmapCache((int) (manager.getMemoryClass() * Config.BITMAP_CACHE_PORTION));
+	}
+
+	public BitmapCache getDefaultBitmapCache() {
+		return bitmapCache;
+	}
+}
