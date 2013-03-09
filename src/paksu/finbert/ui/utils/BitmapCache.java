d7db29a85733596228318ec2ae85111b2d453123
Joonas Lehtonen <lehton83@gmail.com>
2013-03-09 11:18:24 +0200

    added BitmapCache

---
+++ src/paksu/finbert/ui/utils/BitmapCache.java
@@ -0,0 +1,16 @@
+package paksu.finbert.ui.utils;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+public class BitmapCache extends LruCache<String, Bitmap> {
+
+	public BitmapCache(int cacheSizeInMegabytes) {
+		super(cacheSizeInMegabytes * 1024 * 1024);
+	}
+
+	@Override
+	protected int sizeOf(String key, Bitmap value) {
+		return value.getByteCount();
+	}
+}
